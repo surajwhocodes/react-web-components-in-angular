a5c9dc12b3350bdd3acbfccc67bbd58af285bddb
Suraj D Patel <74981496+teeny-tinypen@users.noreply.github.com>
2023-03-16 20:43:02 +0530

    Add files via upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CustomReactWrapperComponent } from 'src/react/CustomReactWrapperComponent';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CustomReactWrapperComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
